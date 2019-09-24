0ede392566bf676bcc6be7c1938bfcc89e028e50
Probiazº <alexy-tobiaz2002@hotmail.com>
2019-09-24 09:43:51 -0500

    df

---
+++ WpfApp1/WpfApp1/Empleado.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    class Empleado
+    {
+        public string Nombre { get; set; }
+        public string Titulo { get; set; }
+        public static Empleado GetEmpleado()
+        {
+            var emp = new Empleado()
+            {
+                Nombre = "Saúl Silva",
+                Titulo = "Todologo"
+            };
+            return emp;
+
+        }
+           
+    }
+}
